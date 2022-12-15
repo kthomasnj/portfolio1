215dce801533ba76bc6a2c6607d5affef18a810d
kthomasnj <kthomasnj@gmail.com>
2022-12-15 16:42:22 -0500

    Footer completed

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -35,7 +35,7 @@ const Contacts = () => {
     }
 
     return (
-        <div className="contacts">
+        <div id="contact" className="contacts">
             <div className="text-center">
                 <h1>contact me</h1>
                 <p>Please fill out the form describing your project needs and I will contact you as soon as possible.</p>
